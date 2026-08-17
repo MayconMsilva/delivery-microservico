85da03021f8e2dc27efa655f664891f320cdc641
JAVA-DIO <mayconmsilva01@gmail.com>
2026-08-17 18:10:06 -0300

    Adicionado Repository do Customer-Service

---
+++ curstomer-service/src/main/java/com/delivery/curstomer_service/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.delivery.curstomer_service.repository;
+
+import com.delivery.curstomer_service.entity.Customer;
+import org.hibernate.internal.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByUserId(Long userId);
+}

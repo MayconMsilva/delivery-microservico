749a843b41e4622b9991840efb3aface9a1f86db
JAVA-DIO <mayconmsilva01@gmail.com>
2026-08-24 19:11:52 -0300

    Adicionado Security Jwt no Microserviço Order-Service

---
+++ order-service/src/main/java/com/delivery/order_service/security/JwtService.java
@@ -0,0 +1,53 @@
+package com.delivery.order_service.security;
+
+import com.delivery.order_service.exception.InvalidTokenException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey;
+
+    public JwtService(@Value("${jwt.secret}") String secret) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public Claims extractClaims(String token) {
+        try {
+            return Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+        } catch (ExpiredJwtException e) {
+            throw new InvalidTokenException("Token expirado");
+        } catch (JwtException e) {
+            throw new InvalidTokenException("Token inválido");
+        }
+    }
+
+    public boolean isValid(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (InvalidTokenException e) {
+            return false;
+        }
+    }
+
+    public Long extractUserId(String token) {
+        return Long.parseLong(extractClaims(token).getSubject());
+    }
+
+    public String extractRole(String token) {
+        return extractClaims(token).get("role", String.class);
+    }
+}

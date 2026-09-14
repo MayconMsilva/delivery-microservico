6c2807e5a062a46b9d9674ac859e38adabd215a1
JAVA-DIO <mayconmsilva01@gmail.com>
2026-09-14 18:45:17 -0300

    Adicionando Repository no Delivery-Service

---
+++ delivery-service/src/main/java/com/delivery/delivery_service/DeliveryRepository.java
@@ -0,0 +1,11 @@
+package com.delivery.delivery_service;
+
+import com.delivery.delivery_service.entity.model.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+
+    Optional<Delivery> findByOrderId(Long orderId);
+}
